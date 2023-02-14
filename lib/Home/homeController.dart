a5780c47d383f1f43051cfb3c68f55ef2ade2b18
ahmedgfouad <aghmedgfouad2020@gmail.com>
2023-02-15 01:40:01 +0200

    second

---
+++ lib/Home/homeController.dart
@@ -0,0 +1,19 @@
+class HomeController{
+  List containerColor=[
+    0xFF056E63
+  ];
+  List textColor=[
+    0xFFFFFFFF
+  ];
+  List text=[
+    "Chair",
+    "Sofa",
+    "Tables",
+  ];
+
+  List chairImage=[
+    "https://i.pinimg.com/474x/c4/c8/4b/c4c84b355ea74ebee9949d60a2d7a21b.jpg",
+    "https://www.atkinandthyme.co.uk/media/catalog/product/cache/ff4df62209228869e2d6e438912332e8/3/_/3_1.jpg",
+  ];
+
+}
